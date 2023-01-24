c48b369aa64437a3e4e6dec1bf364caaceddcbb6
Atharva961 <85989627+Atharva961@users.noreply.github.com>
2023-01-24 21:21:12 +0530

    Time: 536 ms (8.76%), Space: 114 MB (5.07%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    set<vector<int>> s;
+    
+    void solve(vector<int>& nums, int index, vector<int>& temp)
+    {
+        if(index>=nums.size())
+        {
+            if(temp.size()==nums.size())s.insert(temp);
+            return;
+        }
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=-11)
+            {
+                vector<int> p_nums = nums;
+                p_nums[i] = -11;
+                vector<int> p_temp = temp;
+                p_temp.push_back(nums[i]);
+                
+                solve(p_nums, index+1, p_temp);
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> temp = {};
+        solve(nums, 0, temp);
+        vector<vector<int>> ans(s.begin(), s.end());
+        
+        return ans;
+    }
+};
