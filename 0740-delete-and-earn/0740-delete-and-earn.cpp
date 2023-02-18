ca2df643a55be9dbec201fa529837934f70755f2
Atharva961 <85989627+Atharva961@users.noreply.github.com>
2023-02-18 20:07:13 +0530

    Time: 3 ms (99.70%), Space: 11.9 MB (85.55%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& h, int index, vector<int>& dp)
-    {
-        if(index>=h.size())return 0;
-        if(dp[index]!=-1)return dp[index];
-        int ignore = solve(h, index+1, dp);
-        int del = 0;
-        if(h[index]>0)
-        {
-            del = index*h[index] + solve(h, index+2, dp);
-        }
-        return dp[index] = max(ignore, del);
-    }
+    
     int deleteAndEarn(vector<int>& nums) {
         int maxx = 0;
         for(auto i:nums)maxx = max(i, maxx);
         vector<int> h(maxx+1, 0);
         for(auto i:nums)h[i]++;
-        vector<int> dp(maxx+1, -1);
-        return solve(h, 0, dp);
+        vector<int> dp(maxx+3, 0);
+        
+        for(int i=h.size()-1; i>=0; i--)
+        {
+            int ignore = dp[i+1];
+            int del = 0;
+            if(h[i]>0)
+            {
+                del = i*h[i] + dp[i+2];
+            }
+            dp[i] = max(ignore, del);
+        }
+        return dp[0];
     }
 };
