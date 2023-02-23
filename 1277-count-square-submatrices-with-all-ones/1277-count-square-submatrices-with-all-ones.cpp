ed2044bfcd8bab01f7930b9b73f1e1158a7f11e8
Atharva961 <85989627+Atharva961@users.noreply.github.com>
2023-02-24 00:56:05 +0530

    Time: 66 ms (94.81%), Space: 23.6 MB (80.40%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    bool square(vector<vector<int>>& v, int i1, int j1, int w)
-    {
-        for(int i=i1; i<i1+w; i++)
-        {
-            for(int j=j1; j<j1+w; j++)
-            {
-                if(v[i][j]==0)return false;
-            }
-        }
-        return true;
-    }
     int countSquares(vector<vector<int>>& v) {
-        int n = v.size(), m = v[0].size();
-        int maxx = min(n, m);
         int ans = 0;
         
-        for(int w=1; w<=maxx; w++)
+        for(int i=0; i<v.size(); i++)
         {
-            for(int i=0; i<=n-w; i++)
+            for(int j=0; j<v[0].size(); j++)
             {
-                for(int j=0; j<=m-w; j++)
-                {
-                    if(v[i][j]==1)
-                    {
-                        if(square(v, i, j, w))ans++;
-                    }
-                }
+                if(i>0 && j>0 && v[i][j]>0)v[i][j]+=min(v[i-1][j-1], min(v[i-1][j], v[i][j-1]));
+                ans+=v[i][j];
             }
         }
         
