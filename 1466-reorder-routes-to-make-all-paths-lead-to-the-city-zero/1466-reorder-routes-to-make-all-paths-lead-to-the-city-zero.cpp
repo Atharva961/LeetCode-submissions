4d8288736e95efce586d80920b39e2f62e1f68f8
Atharva961 <85989627+Atharva961@users.noreply.github.com>
2023-03-24 16:21:09 +0530

    Time: 805 ms (13.72%), Space: 155.6 MB (16.98%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& arr) {
-        set<vector<int>> s(arr.begin(), arr.end());
+        set<vector<int>> s(arr.begin(), arr.end());// for searching purpose.
+		
+		//Make adjacency list like for a undirected graph.
         vector<vector<int>> adj(n);
         for(auto i:arr)
         {
@@ -12,6 +14,8 @@ public:
         int ans = 0;
         queue<int> q;
         vector<int> visited(n, 0);
+		
+		//Start BFS from 0
         q.push(0);
         visited[0] = 1;
         
@@ -23,6 +27,8 @@ public:
             {
                 if(!visited[i])
                 {
+					// Basically we are moving away from 0 in BFS. 
+					//So if there is a directed edge from current node(f) to next node(i), there is an edge pointing away from 0, in that case increment ans by 1.
                     if(s.find({f, i})!=s.end())ans++;
                     visited[i] = 1;
                     q.push(i);
